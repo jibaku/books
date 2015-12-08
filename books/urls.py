a9a8411bf5fd1c9bccb4a788abe515cb57699f7c
Fabien Schwob <github@x-phuture.com>
2015-12-08 23:45:43 +0100

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from books.views import BooksList
 from books.views import BookDetail
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', BooksList.as_view(), name='books-index'),
     url(r'^(?P<slug>[-\w]+)$', BookDetail.as_view(), name='books-details'),
-)
+]
