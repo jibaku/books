b3f920d1b97d202910327b8f1b44ddfae8910bbe
Fabien Schwob <github@x-phuture.com>
2015-12-08 22:58:46 +0100

    * Making compatible with python3.X (tested with 3.5)

--- books/models.py
+++ books/models.py
@@ -1,11 +1,16 @@
+from __future__ import unicode_literals
+
+from django.contrib.sites.managers import CurrentSiteManager
+from django.contrib.sites.models import Site
 from django.db import models
 from django.db.models import Q
-from django.contrib.sites.models import Site
-from django.contrib.sites.managers import CurrentSiteManager
+from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 import datetime
 
+
+@python_2_unicode_compatible
 class Editor(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(unique=True)
@@ -14,24 +19,32 @@ class Editor(models.Model):
         verbose_name = _('Editor')
         verbose_name_plural = _('Editors')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.name
+
 
 def cover_filename(instance, filename):
     return "books/%s.jpg" % instance.slug
+
 
 class BookManager(models.Manager):
     def published(self):
         now = datetime.datetime.now()
         queryset = self.filter(validated=True)
         queryset = queryset.filter(start_publication_on__lte=now)
-        q = (Q(end_publication_on__isnull=False)&Q(end_publication_on__gt=now)) | Q(end_publication_on__isnull=True)
+        q = (
+            Q(end_publication_on__isnull=False)
+            & Q(end_publication_on__gt=now)
+        ) | Q(end_publication_on__isnull=True)
         queryset = queryset.filter(q)
         return queryset
+
 
 class SiteBookManager(CurrentSiteManager, BookManager):
     pass
 
+
+@python_2_unicode_compatible
 class Book(models.Model):
     site = models.ForeignKey(Site)
 
@@ -51,13 +64,13 @@ class Book(models.Model):
     asin = models.CharField(max_length=20, null=True, blank=True)
     description = models.TextField()
     cover = models.ImageField(upload_to=cover_filename, blank=True)
-    
+
     objects = BookManager()
     on_site = SiteBookManager()
 
     class Meta:
         verbose_name = _('Book')
         verbose_name_plural = _('Books')
-    
-    def __unicode__(self):
+
+    def __str__(self):
         return self.title
